b1970709ac8b839ed73e15a05c1020c072f31fe5
Martin Hosken <martin_hosken@sil.org>
2023-11-06 17:14:42 +0000

    Fix \cp following \c

--- python/lib/usfmtc/usxgrammar.py
+++ python/lib/usfmtc/usxgrammar.py
@@ -20,7 +20,7 @@ alljobs = {
     "List":                 ("list", ("List.para.style.enum",)),
     "ListChar":             ("listchar", ("+char.closed", "ListChar.char.style.enum",)),
     "Milestone":            ("ms", ("MilestoneWithAttrib.enum", "MilestoneWithAttrib.ms.style.qt", "Milestone.enum")),
-    "Para":                 ("p", ("Para.para.style.enum", "Break")),
+    "Para":                 ("p", ("VersePara", "NonVersePara", "Para.para.style.enum", "Para.nonpara.style.enum", "Break")),
     "PeripheralDivision":   ("periph", ("Peripheral.FRT.periph.id.enum", "Peripheral.INT.periph.id.enum",
                                         "Peripheral.BAK.periph.id.enum", "Peripheral.OTH.periph.id.enum", "PeripheralContent")),
     "Scripture":            ("id", ("BookIdentification", "BookIdentification.book.code.enum", "ChapterContent")),
