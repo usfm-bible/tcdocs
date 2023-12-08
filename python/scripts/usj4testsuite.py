fc1447fd639ffc23c1327f50a432ad14717daf36
Martin Hosken <martin_hosken@sil.org>
2023-12-08 15:20:51 +0000

    refactor terminals in the grammar

--- python/scripts/usj4testsuite.py
+++ python/scripts/usj4testsuite.py
@@ -1,12 +1,12 @@
+#!/usr/bin/env python3
 import glob, json
 from lxml import etree
 import usx2usj
 
-usx_paths = glob.glob('../../tests/*/*/origin.xml') + glob.glob('../../tests/*/*/*/origin.xml')
+usx_paths = glob.glob('tests/*/*/origin.xml') + glob.glob('tests/*/*/*/origin.xml')
 
 problem_usxs = [
-    '../../tests/usfmjsTests/esb/origin.xml',  # last verse text given outside of paragraph. 
-    '../../tests/special-cases/empty-attributes/origin.xml',  # attributes treated as text content of marker
+    'tests/usfmjsTests/esb/origin.xml',  # last verse text given outside of paragraph. 
 ]
 
 for usx_path in usx_paths:
@@ -22,11 +22,11 @@ for usx_path in usx_paths:
 
 print("Stats\n------\n")
 
-usfm_paths = glob.glob('../../tests/*/*/origin.usfm') + glob.glob('../../tests/*/*/*/origin.usfm')
+usfm_paths = glob.glob('tests/*/*/origin.usfm') + glob.glob('tests/*/*/*/origin.usfm')
 print(f"USFM samples: {len(usfm_paths)}")
 print(f"USX samples: {len(usx_paths)}")
 
-usj_paths = glob.glob('../../tests/*/*/origin.json') + \
-            glob.glob('../../tests/*/*/*/origin.json')
+usj_paths = glob.glob('tests/*/*/origin-usj.json') + \
+            glob.glob('tests/*/*/*/origin-usj.json')
 print(f"USJ samples generated only for: {len(usj_paths)}")
 print("(Converted only where USX was present and did not have elements with status='invalid' or other issues in it.)")
